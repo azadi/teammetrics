2a68840fd9c2650e7169773fad76d76b1f7143bf
Sukhbir Singh <sukhbir.in@gmail.com>
2012-12-22 20:48:38 -0500

    Handle the case of upstream authors (lines added/ deleted)

--- repository/gitstat.py
+++ repository/gitstat.py
@@ -118,11 +118,6 @@ def fetch_logs(ssh, conn, cur, teams, users):
                         logging.error(detail)
                         continue
                     
-                    # For upstream authors, ignore the number of lines added or deleted.
-                    if no_debian:
-                        added = 0
-                        deleted = 0
-
                     # There are some invalid dates, just skip those commits.
                     try:
                         date = date_raw.split()[0]
@@ -130,8 +125,14 @@ def fetch_logs(ssh, conn, cur, teams, users):
                         logging.warning('Invalid date: %s' % date)
                         logging.error(detail)
                         continue
-                    added = added.strip().split()[0]
-                    deleted = deleted.strip().split()[0]
+
+                    # For upstream authors, ignore the number of lines added or deleted.
+                    if no_debian:
+                        added = 0
+                        deleted = 0
+                    else:
+                        added = added.strip().split()[0]
+                        deleted = deleted.strip().split()[0]
 
                     if each_dir.endswith('.git'):
                         each_dir = each_dir[:-4]
