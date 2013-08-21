47b48403f7c9b11e0de58fcddbdf55496d5674ae
Sukhbir Singh <sukhbir.in@gmail.com>
2013-08-20 20:32:50 -0400

    Update the 'spam filter'

--- spamfilter.py
+++ spamfilter.py
@@ -15,6 +15,7 @@ SPAM_KEYWORDS = (
                 'Pls check this new site',
                 'promotion', 
                 'Tim.com.br',
+                'ImageJ ports on exflpcx18766',
                 )
 
 
